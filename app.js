99b0c3efc51c10093fdee19c5850ed10ac2705ad
skaisanlahti <skaisanlahti@gmail.com>
2020-07-06 18:09:02 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/*=== App overview ===
+Generate 4 number passwords for FFXIV private party finder.
+=============================================================================*/
+const password = document.querySelector(".pw-main__password");
+const generate = document.querySelector(".pw-main__generate");
+
+generate.addEventListener("click", generatePassword);
+window.addEventListener("load", generatePassword);
+
+function generatePassword () {
+    let pw = "";
+
+    for (let i = 0; i < 4; i++){
+        pw += getRandomInteger(0, 9);
+    }
+    password.innerText = pw;
+}
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
